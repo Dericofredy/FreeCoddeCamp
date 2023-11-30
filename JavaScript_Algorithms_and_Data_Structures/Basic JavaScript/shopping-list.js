46ea320d17295c9d4636db760a38e38b125e6a4e
Frederico Caldeira <60188689+Dericofredy@users.noreply.github.com>
2023-11-30 17:00:41 -0300

    Create shopping-list.js
    Lista de compras com array

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic JavaScript/shopping-list.js
@@ -0,0 +1,16 @@
+/* Criar lista de compras
+Crie uma lista de compras na variável myList. A lista deve ser um array multidimensional contendo diversos sub-arrays.
+
+O primeiro elemento em cada sub-array deve conter uma string com o nome do item. O segundo elemento deve ser um número representando a quantidade, ou seja,
+
+["Chocolate Bar", 15]
+Deve haver pelo menos 5 sub-arrays na lista. */
+
+const myList = 
+[
+  ["Sugar", 1],
+  ["Milk",5],
+  ["Choco",1],
+  ["Banana",2],
+  ["Tea",2]
+  ];
