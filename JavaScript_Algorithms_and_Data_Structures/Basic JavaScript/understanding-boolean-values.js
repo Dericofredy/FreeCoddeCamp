91f33eacaa8a28172f2a3c452447ddfd7afee4c0
Frederico Caldeira <60188689+Dericofredy@users.noreply.github.com>
2024-01-08 13:26:04 -0300

    Create understanding-boolean-values.js
    Entender valores booleanos

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic JavaScript/understanding-boolean-values.js
@@ -0,0 +1,23 @@
+/*Entender valores booleanos
+Outro tipo de dado é o booleano. Booleanos podem ser apenas dois valores: true ou false. Eles basicamente são interruptores pequenos, onde true é ligado e false é desligado. Esses dois estados são mutuamente exclusivos.
+
+Observação: valores booleanos nunca são escritos com aspas. As strings "true" e "false" não são booleanos e não tem nenhum significado especial em JavaScript.
+
+Modifique a função welcomeToBooleans para que retorne true ao invés de false.*/
+
+function init() {
+  // Altere apenas o código abaixo desta linha
+
+  return false; // Altere esta linha
+
+  // Altere apenas o código acima desta linha
+}
+
+function welcomeToBooleans() {
+  // Altere apenas o código abaixo desta linha
+
+  return true; // Altere esta linha2
+}
+
+console.log(init()+" imprimindo testes durante o exercício mantendo o false");
+console.log(welcomeToBooleans()+" imprimindo resposta verdade do exercício");
